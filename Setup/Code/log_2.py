34c6696896e4e86f02a50f4a9dcc4e2d7e486e7f
Hagen E. Fritz <hagenfritz@gmail.com>
2019-10-22 14:27:41 -0500

    changed datetime import

--- Setup/Code/log_2.py
+++ Setup/Code/log_2.py
@@ -29,7 +29,7 @@ import os, signal
 import subprocess
 from subprocess import call
 import pprint
-from datetime import datetime
+from datetime
 import csv
 
 # Sensor-specific libraries
@@ -87,7 +87,7 @@ def sps30_scan():
 
 	return 0
 	#return {'pm_n_0p5':pm_n[0],'pm_n_1':pm_n[1],'pm_n_2p5':pm_n[2],'pm_n_4':pm_n[3],'pm_n_10':pm_n[4],'pm_c_1':pm_c[0],'pm_c_2p5':pm_c[1],'pm_c_4':pm_c[2],'pm_c_10':pm_c[3]}
-	
+
 def data_mgmt():
     '''
     Combines and stores sensors' data locally and remotely to AWS S3 bucket.\n
