8ae7cc4f0aaa6c419bf29f868888842cefd00d12
Hagen E. Fritz <hagenfritz@gmail.com>
2019-11-15 17:09:28 -0600

    trying things...

--- Setup/DGS/dgs_usb.py
+++ Setup/DGS/dgs_usb.py
@@ -2,10 +2,12 @@ import os, sys
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyUSB0',9600,timeout=5)
-print(ser.name)
+ser = serial.Serial('/dev/ttyUSB0',baudrate=9600,bytesize=8,stopbits=1,timeout=5)
+ser.open()
+print(ser.isopen())
 while True:
-	x = ser.read()
+	print(ser.out_waiting)
+	x = ser.read(8)
 
 	print(x)
 
