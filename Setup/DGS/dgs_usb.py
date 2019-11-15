d6eb18ea956065144a4664f53de379f612b6f8f4
Hagen E. Fritz <hagenfritz@gmail.com>
2019-11-15 17:02:48 -0600

    changed the reading

--- Setup/DGS/dgs_usb.py
+++ Setup/DGS/dgs_usb.py
@@ -3,11 +3,9 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyUSB0',9600,timeout=5)
+print(ser.name)
 while True:
-	line = ser.readline()
-	if len(line) == 0:
-		exit(1)
+	x = ser.read()
 
-	print(line)
+	print(x)
 
-	
