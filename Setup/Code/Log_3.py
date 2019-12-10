48c692a8f37211ef26a905d4bb254d9485ae5929
Hagen E. Fritz <hagenfritz@gmail.com>
2019-12-10 12:20:55 -0600

    Finalized verbose output

--- Setup/Code/Log_3.py
+++ Setup/Code/Log_3.py
@@ -73,8 +73,8 @@ def sgp30_scan(i2c):
     # Outputting
     if verbose:
         print("-------------------------")
-        print("TVOC (ppb):\t\t"+str(TVOC))
-        print("eCO2 (ppm):\t\t"+str(eCO2))
+        print("TVOC (ppb):\t"+str(TVOC))
+        print("eCO2 (ppm):\t"+str(eCO2))
         print("-------------------------")
     # Return data
     data = {'TVOC': TVOC, 'eCO2': eCO2}
