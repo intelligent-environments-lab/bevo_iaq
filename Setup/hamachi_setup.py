712bea6c6a0a0cfeed639f65a168633763c2e210
Hagen E. Fritz <hagenfritz@gmail.com>
2019-12-11 15:20:34 -0600

    fixed bug and attach works now

--- Setup/hamachi_setup.py
+++ Setup/hamachi_setup.py
@@ -1,6 +1,7 @@
 import os
+import time
 
-nickname = input('Name for this device on Hamachi VPN: ")
+nickname = input('Name for this device on Hamachi VPN: ')
 
 # Removing cloned configurations
 os.system('sudo rm -rf /var/lib/logmein-hamachi/')
@@ -8,6 +9,8 @@ os.system('sudo rm -rf /var/lib/logmein-hamachi/')
 # Loigging in, setting nickname, and attaching
 os.system('sudo hamachi login')
 os.system('sudo hamachi set-nick '+ str(nickname))
+os.system('sudo hamachi attach nagy@utexas.edu')
+time.sleep(5)
 os.system('sudo hamachi attach nagy@utexas.edu')
 
 # Ensuring hamachi starts on power on
