94b3c523e97701e288fb8648157cb6f435b7b8ec
Hagen E. Fritz <hagenfritz@gmail.com>
2019-12-18 10:54:25 -0600

    updated message

--- Setup/Code/email_test.py
+++ Setup/Code/email_test.py
@@ -5,12 +5,13 @@ smtp_server = "smtp.gmail.com"
 sender_email = "IEL.Beacon.Manager@gmail.com"  # Enter your address
 receiver_email = "IEL.Beacon.Manager@gmail.com"  # Enter receiver address
 password = "ZoltanIEL2019"
+name = "Hagen"
 message = """\
 Subject: Hi there
 
-This message is sent from Python."""
+This message is sent from Python for {name}."""
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, message.format(name=name))
