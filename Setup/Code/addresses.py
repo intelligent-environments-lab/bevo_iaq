e8cdce66eac46e840616a1d5e3f463f259325f34
Hagen E. Fritz <hagenfritz@gmail.com>
2020-12-22 16:47:14 -0600

    debugging1

--- Setup/Code/addresses.py
+++ Setup/Code/addresses.py
@@ -21,16 +21,29 @@ co, _, _ = dgs.takeMeasurement("/dev/ttyUSB1")
 
 # getting sensirion sensors
 scd30 = checkSensirion()
-sps30 = checkSensirion(I2C_SLAVE=0x69):
+sps30 = checkSensirion(address=0x69)
 
-def checkSensirion(I2C_SLAVE=0x61, I2C_BUS=1, n=18):
+def checkSensirion(address=0x61, bus=1, n=3):
 	"""
+	Checks connection to Sensirion sensors
+
+	Inputs:
+	- address: address of the sensor
+	- bus: i2c bus address
+	- n: number of bytes to read
+
 
 	"""
 	PIGPIO_HOST = '127.0.0.1'
 	pi = pigpio.pi(PIGPIO_HOST)
 
+	print("Connecting...")
+	time.sleep(0.1)
 	h = pi.i2c_open(I2C_BUS, I2C_SLAVE)
+	print("Connected")
+	print("Reading...")
 	count, data = pi.i2c_read_device(h, n)
-
+	time.sleep(0.1)
+	print("Read")
+	
 	return True
