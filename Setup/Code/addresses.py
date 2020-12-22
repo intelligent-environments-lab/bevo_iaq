05873837f912ae68bf444dee496f315a023d9be7
Hagen E. Fritz <hagenfritz@gmail.com>
2020-12-22 16:40:46 -0600

    updatesd

--- Setup/Code/addresses.py
+++ Setup/Code/addresses.py
@@ -2,8 +2,7 @@
 from board import SCL, SDA
 from busio import I2C
 import dgs
-import scd30
-import sps30
+import pigpio
 import adafruit_sgp30 as sgp30
 import adafruit_tsl2591 as tsl2591
 
@@ -21,5 +20,17 @@ no2, _, _ = dgs.takeMeasurement("/dev/ttyUSB0")
 co, _, _ = dgs.takeMeasurement("/dev/ttyUSB1")
 
 # getting sensirion sensors
-_, _, co2 = scd30.takeMasurement()
-n, m = sps30.takeMasurement()
+scd30 = checkSensirion()
+sps30 = checkSensirion(I2C_SLAVE=0x69):
+
+def checkSensirion(I2C_SLAVE=0x61, I2C_BUS=1, n=18):
+	"""
+
+	"""
+	PIGPIO_HOST = '127.0.0.1'
+	pi = pigpio.pi(PIGPIO_HOST)
+
+	h = pi.i2c_open(I2C_BUS, I2C_SLAVE)
+	count, data = pi.i2c_read_device(h, n)
+
+	return True
