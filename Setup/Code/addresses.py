0c2008a459c826d2e6c459bfe7472c77b1863556
Hagen E. Fritz <hagenfritz@gmail.com>
2020-12-26 11:06:04 -0600

    changed output

--- Setup/Code/addresses.py
+++ Setup/Code/addresses.py
@@ -12,7 +12,7 @@ import numpy as np
 import os
 import time
 
-def getI2CAddresses():
+def getStatus():
     i2c = I2C(SCL, SDA)
     addrs = i2c.scan()
     time.sleep(1) # sleep to catch any missed addresses
@@ -25,70 +25,100 @@ def getI2CAddresses():
     except FileNotFoundError:
         print("Cannot find file")
 
-    print("Available Addresses:")
     for addr in all_addrs:
         try:
             name = known_addrs[known_addrs["Hex"] == str(hex(addr))]["Sensor"].values[0]
+            if name in ["SCD30","SPS30"]:
+                status = checkSensirion(hex(addr))
+            elif name in ["SGP30","TSL2591"]:
+                status = checkAdafruit(name)
         except:
             name = ""
-        print(f"\t{hex(addr)}\t{name}")
+            status = ""
+
+        print(f"\t{hex(addr)}\t{name}\t{status}")
 
 def readingOutput(measurment,threshold):
     """
     Outputs an error or success message
     """
     if measurment <= threshold:
-        print("\tERROR READING FROM SENSOR")
+        return False
     else:
-        print("\tDATA READ")
+        #print("\tDATA READ")
         return True
 
     return False
 
-def checkAdafruit(sensor_name="sgp30"):
+def checkAdafruit(sensor_name="SGP30"):
     """
     Checks connection to Adafruit sensors
     """
     # creating i2c bus
     i2c = I2C(SCL, SDA)
 
-    if sensor_name == "sgp30":
-        sgp = sgp30.Adafruit_SGP30(i2c)
-        time.sleep(1)
-        print("Connected to device at 0x70")
-        # Getting measurment
-        sgp.iaq_init()
-        time.sleep(1)
-        _, tvoc = sgp.iaq_measure()
-        read = readingOutput(tvoc,0)
-        if read:
-            print("\tSVM30 READY")
-    elif sensor_name == "tsl2591":
-        tsl = tsl2591.TSL2591(i2c)
-        time.sleep(1)
-        print("Connected to device at 0x29")
-        # Getting measurment
-        tsl.enabled = True
-        time.sleep(1)
+    if sensor_name == "SGP30":
         try:
+            sgp = sgp30.Adafruit_SGP30(i2c)
+        except:
+            return "No Sensor Detected"
+
+        time.sleep(1)
+        #print("Connected to device at 0x70")
+        # Getting measurment
+        try:
+            sgp.iaq_init()
+            time.sleep(1)
+            _, tvoc = sgp.iaq_measure()
+            read = readingOutput(tvoc,0)
+            if read:
+                #print("\tSVM30 READY")
+                pass
+        except:
+            return "Cannot Read Data"
+    elif sensor_name == "tsl2591":
+        try:
+            tsl = tsl2591.TSL2591(i2c)
+        except:
+            return "No Sensor Detected"
+
+        time.sleep(1)
+        #print("Connected to device at 0x29")
+        # Getting measurment
+        try:
+            tsl.enabled = True
+            time.sleep(1)
             lux = tsl.lux
             read = readingOutput(lux,-1)
             if read:
-                print("\tTSL2591 READY")
+                #print("\tTSL2591 READY")
+                pass
         except RuntimeError:
-            print("\tERROR READING FROM SENSOR - Overflow")
+            return "Overflow"
 
     else:
         print(f"Sensor {sensor_name} does not exist.")
+
+    return "Ready"
 
 def checkDGS(dev_no=0):
     """
     Checks connection to DGS sensors
     """
-    c, _, _ = dgs.takeMeasurement(f"/dev/ttyUSB{dev_no}")
-    read = readingOutput(float(c),-10)
-    if read:
-        print(f"\tSPEC{dev_no} READY")
+    try:
+        c, _, _ = dgs.takeMeasurement(f"/dev/ttyUSB{dev_no}")
+    except:
+        return "No Sensor Detected"
+
+    try:
+        read = readingOutput(float(c),-10)
+        if read:
+            #print(f"\tSPEC{dev_no} READY")
+            pass
+    except:
+        return "Cannot Read Data"
+
+    return "Ready"
 
 def checkSensirion(address=0x61, bus=1, n=3):
     """
@@ -99,25 +129,31 @@ def checkSensirion(address=0x61, bus=1, n=3):
     - bus: i2c bus address
     - n: number of bytes to read
     """
-    PIGPIO_HOST = '127.0.0.1'
-    pi = pigpio.pi(PIGPIO_HOST)
+    # Connect to sensor
+    try:
+        PIGPIO_HOST = '127.0.0.1'
+        pi = pigpio.pi(PIGPIO_HOST)
+        h = pi.i2c_open(bus, address)
+    except Exception as inst:
+        #print("Connected to device at", address)
+        return "No Sensor Detected"
 
-    h = pi.i2c_open(bus, address)
-    print("Connected to device at", address)
     count, data = pi.i2c_read_device(h, n)
     if data:
-        print("\tDATA READ")
-        return True
+        #print("\tDATA READ")
+        pass
     else:
-        print("\tERROR READING FROM SENSOR")
+        #print("\tERROR READING FROM SENSOR")
+        return "Cannot Read Data"
 
     h.i2c_close()
     pi.stop()
-    return False
+    return "Ready"
 
 def main():
-    getI2CAddresses()
+    getStatus()
 
+def old():
     # getting the adafruit sensors
     for sensor in ["sgp30","tsl2591"]:
         time.sleep(1)
