9bb54686c59b3547efd9820d117dca159eb57862
Hagen E. Fritz <hagenfritz@gmail.com>
2021-01-15 10:18:10 -0600

    indentation

--- Setup/Code/display.py
+++ Setup/Code/display.py
@@ -7,23 +7,23 @@ import adafruit_ssd1306
 i2c = io.I2C(board.SCL, board.SDA)
 
 # creating sensor object (height, width, i2c object, address (optional))
-oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
 #oled.fill(1)
 #oled.show()
 
 def main():
-	disp = adafruit_ssd1306.SSD1306_128_64(rst=None)
+    disp = adafruit_ssd1306.SSD1306_128_64(rst=None)
     disp.begin()
     disp.clear()
     disp.display()
-    oled.fill(0)
+    disp.fill(0)
     disp.display()
 
     # Setting up display parameters
     small_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 10)
-	large_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 30)
-	unit_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
+    large_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 30)
+    unit_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
 
 if __name__ == '__main__':
     main()
