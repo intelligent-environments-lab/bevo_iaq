fec5dbfb3e22571865aa1640b910a4f47fcfad45
Hagen E. Fritz <hagenfritz@gmail.com>
2021-01-15 10:20:52 -0600

    not using ww code

--- Setup/Code/display.py
+++ Setup/Code/display.py
@@ -3,22 +3,21 @@ import busio as io
 
 import adafruit_ssd1306
 
-# creating i2c instance
-i2c = io.I2C(board.SCL, board.SDA)
 
-# creating sensor object (height, width, i2c object, address (optional))
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
 #oled.fill(1)
 #oled.show()
 
 def main():
-    disp = adafruit_ssd1306.SSD1306_128_64(rst=None)
-    disp.begin()
+    # creating i2c instance
+    i2c = io.I2C(board.SCL, board.SDA)
+
+    # creating sensor object (height, width, i2c object, address (optional))
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
     disp.clear()
-    disp.display()
-    disp.fill(0)
-    disp.display()
+    disp.fill(1)
+    disp.show()
 
     # Setting up display parameters
     small_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 10)
