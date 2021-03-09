4ea8d2bf5db83356f6fa799ecb8717ac6f5f8844
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-03-09 12:27:52 -0600

    Create bevo_iaq_setup_dev.sh

---
+++ Setup/bevo_iaq_setup_dev.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update
+sudo apt-get upgrade
+
+# Additional apt packages
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-venv -y
+sudo apt-get install -y i2c-tools
+
+# Virtual Environment Setup
+rm -rf ~/bevo_iaq/.venv
+mkdir ~/bevo_iaq/.venv
+cd ~/bevo_iaq/
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install addtional packages
+pip install -r Setup/requirements.txt
+
+# Github Credentials
+git config --global user.email "hagenfritz@utexas.edu"
+git config --global user.name "hagenfritz"
+
+# Set up locale, timezone, language
+sudo timedatectl set-timezone US/Central
