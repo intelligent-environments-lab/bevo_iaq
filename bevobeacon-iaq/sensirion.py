915356adb662306ad0ee7e107a91fae6c6f97a5b
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-03-14 19:10:56 -0500

    add new libraries

--- bevobeacon-iaq/sensirion.py
+++ bevobeacon-iaq/sensirion.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-import sps30
-import scd30
+from scd30_i2c import SCD30 as Sensirion_SCD30
+
+from sps30 import SPS30 as Sensirion_SPS30
+import time
+from time import sleep
 
 class SPS30:
+    # https://pypi.org/project/sps30/
     def __init__(self) -> None:
-        pass
+        sps = Sensirion_SPS30(1)
+        sps.start_measurement()
+        self.sps = sps
 
     def scan(self):
         '''
@@ -14,18 +20,26 @@ class SPS30:
         Returns dictionary containing counts for 0.5, 1, 2.5 , 4, and 10 microns
         in diameter and concentrations for 1, 2.5, 4, and 10 microns in diameter.
         '''
-        try:
-            pm_n, pm_c = sps30.takeMeasurement()
-        except:
-            pm_n = [np.nan,np.nan,np.nan,np.nan,np.nan]
-            pm_c = [np.nan,np.nan,np.nan,np.nan]
+        sps = self.sps
+        while not sps.read_data_ready_flag():
+            time.sleep(0.2)
 
-        return {'pm_n_0p5':pm_n[0],'pm_n_1':pm_n[1],'pm_n_2p5':pm_n[2],'pm_n_4':pm_n[3],'pm_n_10':pm_n[4],'pm_c_1':pm_c[0],'pm_c_2p5':pm_c[1],'pm_c_4':pm_c[2],'pm_c_10':pm_c[3]}
+        sps.read_measured_values()
+        pm = sps.dict_values
+
+        return {'pm_n_0p5':pm['nc0p5'],'pm_n_1':pm['nc1p0'],'pm_n_2p5':pm['nc2p5'],'pm_n_4':pm['nc4p0'],'pm_n_10':pm['nc10p0'],'pm_c_1':pm['pm1p0'],'pm_c_2p5':pm['pm2p5'],'pm_c_4':pm['pm4p0'],'pm_c_10':pm['pm10p0']}
 
 
 class SCD30:
+    
+    # https://pypi.org/project/scd30-i2c/
     def __init__(self) -> None:
-        pass
+        
+        scd30 = Sensirion_SCD30()
+
+        scd30.set_measurement_interval(2)
+        scd30.start_periodic_measurement()
+        self.scd30 = scd30
     
     def scan(self):
         '''
@@ -35,9 +49,11 @@ class SCD30:
         the temperature in degress Celsius, and the relative humidity as a 
         percent.
         '''
-
+        while not scd.get_data_ready():
+            time.sleep(0.2)
+            
         try:
-            tc, rh, co2 = scd30.takeMeasurement()
+            co2, tc, rh = self.scd30.read_measurement()
         except:
             co2 = np.nan
             tc = np.nan
