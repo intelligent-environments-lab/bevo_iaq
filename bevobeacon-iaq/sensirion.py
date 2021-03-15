9a20767b561816481f090fcaff911d85aa32390b
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-03-14 19:37:40 -0500

    Update requirements, add exceptions

--- bevobeacon-iaq/sensirion.py
+++ bevobeacon-iaq/sensirion.py
@@ -6,6 +6,7 @@ from sps30 import SPS30 as Sensirion_SPS30
 import time
 from time import sleep
 
+
 class SPS30:
     # https://pypi.org/project/sps30/
     def __init__(self) -> None:
@@ -14,45 +15,67 @@ class SPS30:
         self.sps = sps
 
     def scan(self):
-        '''
+        """
         Measures different particulate matter counts and concentrations in the
         room. Data are stored locally and to AWS S3 bucket.
         Returns dictionary containing counts for 0.5, 1, 2.5 , 4, and 10 microns
         in diameter and concentrations for 1, 2.5, 4, and 10 microns in diameter.
-        '''
+        """
         sps = self.sps
         while not sps.read_data_ready_flag():
             time.sleep(0.2)
+        try:
+            sps.read_measured_values()
+            pm = sps.dict_values
+        except:
+            pm = {
+                "nc0p5": np.nan,
+                "nc1p0": np.nan,
+                "nc2p5": np.nan,
+                "nc4p0": np.nan,
+                "nc10p0": np.nan,
+                "pm1p0": np.nan,
+                "pm2p5": np.nan,
+                "pm4p0": np.nan,
+                "pm10p0": np.nan,
+            }
 
-        sps.read_measured_values()
-        pm = sps.dict_values
-
-        return {'pm_n_0p5':pm['nc0p5'],'pm_n_1':pm['nc1p0'],'pm_n_2p5':pm['nc2p5'],'pm_n_4':pm['nc4p0'],'pm_n_10':pm['nc10p0'],'pm_c_1':pm['pm1p0'],'pm_c_2p5':pm['pm2p5'],'pm_c_4':pm['pm4p0'],'pm_c_10':pm['pm10p0']}
+        return {
+            "pm_n_0p5": pm["nc0p5"],
+            "pm_n_1": pm["nc1p0"],
+            "pm_n_2p5": pm["nc2p5"],
+            "pm_n_4": pm["nc4p0"],
+            "pm_n_10": pm["nc10p0"],
+            "pm_c_1": pm["pm1p0"],
+            "pm_c_2p5": pm["pm2p5"],
+            "pm_c_4": pm["pm4p0"],
+            "pm_c_10": pm["pm10p0"],
+        }
 
 
 class SCD30:
-    
+
     # https://pypi.org/project/scd30-i2c/
     def __init__(self) -> None:
-        
+
         scd30 = Sensirion_SCD30()
 
         scd30.set_measurement_interval(2)
         scd30.start_periodic_measurement()
         self.scd30 = scd30
-    
+
     def scan(self):
-        '''
+        """
         Measures the carbon dioxide concentration, temperature, and relative
         humidity in the room. Data are stored locally and to AWS S3 bucket.
         Returns a dictionary containing the carbon dioxide concentration in ppm,
-        the temperature in degress Celsius, and the relative humidity as a 
+        the temperature in degress Celsius, and the relative humidity as a
         percent.
-        '''
+        """
         scd30 = self.scd30
         while not scd30.get_data_ready():
             time.sleep(0.2)
-            
+
         try:
             co2, tc, rh = scd30.read_measurement()
         except:
@@ -60,4 +83,4 @@ class SCD30:
             tc = np.nan
             rh = np.nan
 
-        return {'CO2':co2,'TC':tc,'RH':rh}
+        return {"CO2": co2, "TC": tc, "RH": rh}
