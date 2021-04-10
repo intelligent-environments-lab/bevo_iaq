a86b7ae6421ac4c6fb835fd1f4714d12443516dd
Hagen E. Fritz <hagenfritz@gmail.com>
2021-04-09 21:05:02 -0500

    fixed path

--- fix_number.sh
+++ fix_number.sh
@@ -2,5 +2,4 @@
 
 str=$1
 
-sed -i "s/beacon = '00'/beacon = '$str'/g" /home/pi/bevo_iaq/Setup/Code/log_3.py
-sed -i "s/beacon = '00'/beacon = '$str'/g" /home/pi/bevo_iaq/Setup/Code/log_2.py
+sed -i "s/beacon = '00'/beacon = '$str'/g" /home/pi/bevo_iaq/bevobeacon-iaq/main.py
