27002950670076de9472f7e3a542a14b788201c5
Hagen Fritz <33231914+HagenFritz@users.noreply.github.com>
2021-04-13 11:50:02 -0500

    user added

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ python3 -m venv ~/bevo_iaq/.venv
 source ~/bevo_iaq/.venv/bin/activate
 
 # Install additional packages
-pip install -r ~/bevo_iaq/bevobeacon-iaq/requirements.txt
+pip install -r ~/bevo_iaq/bevobeacon-iaq/requirements.txt --user
 
 # Github Credentials
 git config --global user.email "hagenfritz@utexas.edu"
