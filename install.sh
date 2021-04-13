59f7fb1c50b5dec32994a7c4e5c1d5a75f27f574
Hagen Fritz <33231914+HagenFritz@users.noreply.github.com>
2021-04-13 11:22:19 -0500

    added permissions

--- install.sh
+++ install.sh
@@ -38,9 +38,9 @@ sudo timedatectl set-timezone US/Central
 
 for s in display sensors; do
 	sudo cp startup/${s}.service /lib/systemd/system/${s}.service
- 	systemctl enable ${s}
+ 	sudo systemctl enable ${s}
 done
 
 sudo cp startup/bevobeacon.service /lib/systemd/system/bevobeacon.service
-systemctl enable bevobeacon
-systemctl start bevobeacon
+sudo systemctl enable bevobeacon
+sudo systemctl start bevobeacon
