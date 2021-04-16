abf86164181c345b1172fe699ca2655f2e966a9b
Hagen E. Fritz <hagenfritz@gmail.com>
2021-04-16 10:15:56 -0500

    removed git and install functionality

--- startup/cleanup.py
+++ startup/cleanup.py
@@ -11,14 +11,6 @@ class clean():
             os.system(f"sudo rm /etc/systemd/system/{service}.service")
             os.system(f"sudo rm /lib/systemd/system/{service}.service")
 
-    def update_from_git(self):
-        """pulls from git"""
-        os.system("cd ~/bevo_iaq && git reset --hard && git pull")
-
-    def install_new_software(self):
-        """runs the installation"""
-        os.system("cd ~/bevo_iaq/ && sh install.sh")
-
     def update_beacon_number(self):
         """updates the beacon number from user prompt"""
         bb = input("Beacon Number (include zero for numbers less than 10): ")
@@ -33,10 +25,6 @@ class clean():
         """runs all the functions with"""
         print("REMOVING OLD SERVICE FILES")
         self.remove_old_services()
-        #print("UPDATING FROM GIT")
-        #self.update_from_git()
-        print("INSTALLING NEW SOFTWARE")
-        self.install_new_software()
         print("UPDATING BEACON NUMBER")
         self.update_beacon_number()
         print("REMOVING OLD DATA DIRECTORIES")
