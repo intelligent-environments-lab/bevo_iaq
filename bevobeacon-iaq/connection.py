bb1d329d0af52cc5c38f187497c11e5d6218d2ad
Hagen E. Fritz <hagenfritz@gmail.com>
2021-07-20 13:28:02 -0500

    really fixed

--- bevobeacon-iaq/connection.py
+++ bevobeacon-iaq/connection.py
@@ -9,9 +9,9 @@ GPIO.setwarnings(False)
 GPIO.setup(led_pin,GPIO.OUT)
 
 while True:
-    ps = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    ps = subprocess.Popen(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     try:
-        output = subprocess.check_output(('grep', 'off'), stdin=ps.stdout)
+        output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
         status = "Not Connected"
     except subprocess.CalledProcessError:
         # grep did not match any lines
