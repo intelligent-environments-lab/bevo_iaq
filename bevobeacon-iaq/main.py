5eab75f4283f2f7ee0b8ca4d58c7362e438f46f2
Hagen E. Fritz <hagenfritz@gmail.com>
2022-03-03 15:55:32 -0600

    catch aws errors

--- bevobeacon-iaq/main.py
+++ bevobeacon-iaq/main.py
@@ -28,9 +28,16 @@ from botocore.exceptions import ClientError
 
 async def main(beacon = '00'):
     # AWS credentials
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    BUCKET_NAME = os.environ['BUCKET_NAME']
+    try:
+        AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+        AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+        BUCKET_NAME = os.environ['BUCKET_NAME']
+    except KeyError:
+        # credentials have not been specified
+        AWS_ACCESS_KEY_ID = ""
+        AWS_SECRET_ACCESS_KEY = ""
+        BUCKET_NAME = ""
+
     s3 = boto3.client(
         's3',
         aws_access_key_id = AWS_ACCESS_KEY_ID,
